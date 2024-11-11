5949e86d30d3a0acca5ac9d17a4ca8d785d72b3e
juneisjune <yhs1535@naver.com>
2024-11-11 17:55:21 +0900

    [Feat] add google addsence code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import TimeCalculator from './TimeCalculator';
+import AdsenseComponent from './components/AdsenseComponent';
+import './App.css'; // 스타일을 적용하기 위해 CSS 파일을 불러옵니다.
 
 function App() {
   return (
-    <div>
-      <TimeCalculator />
+    <div className="app-container">
+      {/* 왼쪽 광고 */}
+      <div className="ad-left">
+        <AdsenseComponent adClient="ca-pub-6428134824213344" adSlot="1571265083" />
+      </div>
+      
+      {/* 중앙 시간 계산기 */}
+      <div className="calculator">
+        <TimeCalculator />
+      </div>
+      
+      {/* 오른쪽 광고 */}
+      <div className="ad-right">
+        <AdsenseComponent adClient="ca-pub-6428134824213344" adSlot="1571265083" />
+      </div>
     </div>
   );
 }
